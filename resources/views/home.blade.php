8c38e6f781f889ebbc05ea822857872939fdd2a1
Hasnah <hasnah.ghani@icu.gov.my>
2024-10-15 15:16:25 +0800

    paginya

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,13 @@
 @section('content')
 
     @php($_name=$name ?? "team")
-    {{--
-    1. if name equal to abu , i want to display . You are banned
-    --}}
 
-   @if($_name == "abu")
+    @if($_name == "abu")
       <p>You are banned.</p>
-   @elseif($_name == "hasnah")
+    @elseif($_name == "hasnah")
       <p>You are beautiful.</p>
-   @else
-       <h1>Hello,  {{ $_name }}.</h1>
-   @endif
-   <button type="button" class="btn btn-danger">Klik Sini</button>
+    @else
+      <h1>Hello,  {{ $_name }}.</h1>
+    @endif
+      <button type="button" class="btn btn-danger">Klik Sini</button>
 @endsection
